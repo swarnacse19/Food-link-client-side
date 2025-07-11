e73412927d001f4e680a3653888e6d005b138b02
swarnacse19 <sharna.cse9.bu@gmail.com>
2025-07-11 12:37:57 +0600

    create footer section

--- src/pages/shared/Footer.jsx
+++ src/pages/shared/Footer.jsx
@@ -1,54 +1,65 @@
 import React from "react";
+import { FaTwitter, FaLinkedin, FaPhoneAlt } from "react-icons/fa";
+import { IoLogoFacebook } from "react-icons/io";
+import { MdEmail } from "react-icons/md";
+import { Link } from "react-router";
 
 function Footer() {
   return (
-    <footer className="footer footer-horizontal footer-center bg-neutral text-neutral-content p-10">
-      <aside>
-        {/* <ProFastLogo></ProFastLogo> */}
-        <p className="font-bold">
-          ACME Industries Ltd.
-          <br />
-          Providing reliable tech since 1992
-        </p>
-        <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
-      </aside>
-      <nav>
-        <div className="grid grid-flow-col gap-4">
-          <a>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              className="fill-current"
-            >
-              <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-            </svg>
-          </a>
-          <a>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              className="fill-current"
-            >
-              <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-            </svg>
-          </a>
-          <a>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              className="fill-current"
-            >
-              <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-            </svg>
-          </a>
+    <footer className="bg-[#FF8C42] text-black text-[14px] px-16 py-14">
+      <div className="max-w-7xl mx-auto flex flex-col gap-8 md:flex-row justify-between">
+        <div>
+          <h1 className="text-3xl font-bold mb-3">FoodLink</h1>
+          <div className="flex gap-2 items-center mb-4">
+            <FaPhoneAlt />
+            <p>Cell/WhatsApp: +880 18243-11959</p>
+          </div>
+          <div className="flex gap-2 items-center">
+            <MdEmail />
+            <p>Email: swarna.bu11@gmail.com</p>
+          </div>
         </div>
-      </nav>
+
+        <div>
+          <h3 className="text-xl font-semibold mb-2">Important Links</h3>
+          <ul className="space-y-1">
+            <li>
+              <Link to="/about" className="hover:underline">
+                About Us
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className="hover:underline">
+                Contact Us
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className="hover:underline">
+                Terms & Conditions
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h3 className="text-xl font-semibold mb-2">Follow Us</h3>
+          <div className="flex space-x-4">
+            <a href="#" className="">
+              <FaTwitter size={24} />
+            </a>
+            <a href="#" className="">
+              <FaLinkedin size={24} />
+            </a>
+            <a href="#" className="">
+              <IoLogoFacebook size={24} />
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="border-t border-gray-700 mt-8 pt-4 text-center text-sm">
+        © 2025 FoodLink. All rights reserved.
+      </div>
     </footer>
   );
 }
