36af61830857d68b2e48d66a6036edc06927bc73
swarnacse19 <sharna.cse9.bu@gmail.com>
2025-07-18 19:05:57 +0600

    latest charity request added on home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,12 +3,14 @@ import MissionSlider from './MissionSlider';
 import Featured from './Featured';
 import ImpactStats from './ImpactStats';
 import Community from './Community';
+import LatestCharityRequests from './LatestCharityRequest';
 
 function Home() {
   return (
     <div>
       <MissionSlider></MissionSlider>
       <Featured></Featured>
+      <LatestCharityRequests></LatestCharityRequests>
       <ImpactStats></ImpactStats>
       <Community></Community>
     </div>
---
+++ src/pages/Home/LatestCharityRequest.jsx
@@ -0,0 +1,48 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxios from "../../hooks/useAxios";
+
+const LatestCharityRequests = () => {
+  const axiosInstance = useAxios();
+
+  const { data: latestRequests = [], isLoading } = useQuery({
+    queryKey: ["latest-charity-requests"],
+    queryFn: async () => {
+      const res = await axiosInstance.get("/latest-charity-requests");
+      return res.data;
+    },
+  });
+
+  if (isLoading) return <p>Loading recent requests...</p>;
+
+  return (
+    <div className="my-10 px-4 max-w-11/12 mx-auto">
+      <h2 className="text-3xl text-center font-bold mb-6">Latest Charity Requests</h2>
+      <div className="grid md:grid-cols-3 gap-6">
+        {latestRequests.map((req) => (
+          <div
+            key={req._id}
+            className="bg-white shadow-lg p-7 rounded-xl border"
+          >
+            <div className="flex items-center gap-3 mb-3">
+              <img
+                src={req.charityImage || "/default-avatar.png"}
+                alt={req.charityName}
+                className="w-12 h-12 rounded-full object-cover"
+              />
+              <div>
+                <p className="font-semibold">{req.charityName}</p>
+                <p className="text-xs text-gray-500">{req.charityEmail}</p>
+              </div>
+            </div>
+            <h4 className="font-bold text-sm text-gray-700">
+              Donation: {req.donationTitle}
+            </h4>
+            <p className="text-sm mt-2 text-gray-600">{req.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LatestCharityRequests;
